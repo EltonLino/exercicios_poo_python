d06d959b2ceb4e052a8d8a582b14d55d7b34d573
Elton Lino <eltonblino@gmail.com>
2025-10-20 05:51:30 -0300

    exercicios polimorfismo

---
+++ exercicio_carro2/carro.py
@@ -0,0 +1,15 @@
+from veiculo import Veiculo
+
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, cor):
+        super().__init__(marca, modelo)
+        self._cor = cor
+
+
+    def ligar(self):
+        print(f'O carro {self._modelo} está ligado.')
+
+
+    def __str__(self):
+        return f'Marca: {self._marca}'
+
---
+++ exercicio_carro2/main.py
@@ -0,0 +1,9 @@
+from carro import Carro
+
+carro1 = Carro('Chevrolet', 'Onix', 'Preto')
+carro2 = Carro('Volwsvagem', 'Fox', 'Prata')
+carro3 = Carro('Fiat', 'Palio', 'Vermelho')
+
+print(carro1)
+print(carro2)
+print(carro3)
